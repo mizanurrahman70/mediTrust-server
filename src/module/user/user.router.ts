679b7e682c3e4fde1b30fe5a313118b20abc7342
MD. SHIPON <sshimi910@gmail.com>
2025-04-14 16:24:06 -0800

    no changes

--- src/module/user/user.router.ts
+++ src/module/user/user.router.ts
@@ -2,7 +2,6 @@ import { Router } from 'express'
 import { userController } from './user.contoller'
 import auth from '../../middlewares/auth'
 
-
 const userRouter = Router()
 userRouter.get('/:userId', userController.getSingleUser)
 userRouter.put('/:userId', userController.updateUser)
